9600545dcf7529bab32afbd2f224fd361661f05f
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-06 09:14:20 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+void main(){
+  ///Responsive and Adaptive- Represent the Application in different sizes of Screens..
+  ///Example- TV,Desktop,Laptop,Phone,Tablet
+  ///Responsive- We Use Media Queries,Flexible Grid,Row,Column etc for that (One Template for all devices)
+  ///Adaptive- Using different components for different devices (Different Components for different devices)
+  ///Used Packages and Widgets- Aspect Ratio,Media Query,Expanded,Flexible,Fractionally Sized Box,Layout Builder,Responsive Grid etc..
+}
