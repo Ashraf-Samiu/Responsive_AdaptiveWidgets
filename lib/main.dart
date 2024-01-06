826cf464554d7d755eccbc5477e6c4fb872b9e42
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-06 12:44:25 +0600

    Flutter_BootStrap Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_grid/responsive_grid.dart';
+import 'package:flutter_bootstrap/flutter_bootstrap.dart';
 void main(){
   runApp(MyApp());
 }
@@ -15,67 +15,103 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     ///More Documentation in pub.dev...
-    ///Responsive-Grid Installation:
-    ///step-1/command- flutter pub add responsive_grid
-    ///step-2/dependencies: responsive_grid: ^2.4.4
-    ///step-3/import 'package:responsive_grid/responsive_grid.dart';
+    ///Flutter_BootStrap Installation:
+    ///step-1/command- flutter pub add flutter_bootstrap
+    ///step-2/dependencies: flutter_bootstrap: ^2.0.0
+    ///step-3/import 'package:flutter_bootstrap/flutter_bootstrap.dart';
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
       ),
       body: SingleChildScrollView(
-        child: ResponsiveGridRow(
+        child: BootstrapContainer(
           children: [
-            ResponsiveGridCol(
-                xl: 1,///extra large-(TV/widescreen monitor)
-                ///lg= >1200||1140 (max container width)
-                lg: 2,///large device-(Computer Monitor Screen)
-                ///lg= <1200||960(>992) (max container width)
-                md: 3,///medium device-(laptop Screen)
-                ///md= <992||960 (max container width)
-                sm: 12,///small device-(Tablet)
-                ///sm= <768px||720 (max container width)
-                xs: 12,///extra small-(mobile phone)
-                ///xs= <576px||540 (max container width)
-                child: Container(
-                  color: Colors.redAccent,
-                  height: 300,
-                )
-            ),
-            ResponsiveGridCol(
-                xl: 6, lg: 3, md: 4,sm: 2,xs: 4,
-                child: Container(
-                  color: Colors.amberAccent,
-                  height: 300,
-                )
-            ),
-            ResponsiveGridCol(
-                xl: 6, lg: 3, md: 4,sm: 2,xs: 4,
-                child: Container(
-                  color: Colors.blueAccent,
-                  height: 300,
-                )
-            ),
-            ResponsiveGridCol(
-                xl: 6, lg: 3, md: 4,sm: 2,xs: 4,
-                child: Container(
-                  color: Colors.greenAccent,
-                  height: 300,
-                )
-            ),
-            ResponsiveGridCol(
-                xl: 6, lg: 3, md: 4,sm: 2,xs: 4,
-                child: Container(
-                  color: Colors.purpleAccent,
-                  height: 300,
-                )
-            ),
-            ResponsiveGridCol(
-                xl: 6, lg: 3, md: 4,sm: 2,xs: 4,
-                child: Container(
-                  color: Colors.lightBlue,
-                  height: 300,
-                )
+            BootstrapRow(children: [
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.redAccent,
+                    height: 200,
+                  )
+                ),
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.blueAccent,
+                    height: 200,
+                  )
+              ),
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.greenAccent,
+                    height: 200,
+                  )
+              ),
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.purpleAccent,
+                    height: 200,
+                  )
+              ),
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.deepOrange,
+                    height: 200,
+                  )
+              ),
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.lightGreen,
+                    height: 200,
+                  )
+                ),
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.red,
+                    height: 200,
+                  )
+              ),
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.brown,
+                    height: 200,
+                  )
+              ),
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.indigo,
+                    height: 200,
+                  )
+              ),
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.black45,
+                    height: 200,
+                  )
+              ),
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.cyan,
+                    height: 200,
+                  )
+              ),
+              BootstrapCol(
+                  sizes: "col-6 col-sm-4 col-md-6 col-lg-2 col-xl-6",
+                  child: Container(
+                    color: Colors.lime,
+                    height: 200,
+                  )
+                ),
+              ]
             ),
           ],
         ),
