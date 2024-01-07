389bf439b78a6a4172c0dde107dd1562be1675d5
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-08 03:55:50 +0600

    Wrap Commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,81 +18,17 @@ class HomeScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text("Home"),
       ),
-      body: LayoutBuilder(
-          ///used for Adaptive..
-          builder: (context, BoxConstraints constraints){
-        if(constraints.maxWidth<300){
-          return Container(
-            width: 300,
-            height: 300,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.deepOrange,
-              border: Border.all(
-                color: Colors.black,
-                width: 5
-              )
-            ),
-              child: Text("Tiny Device"),
-          );
-        } else if(constraints.maxWidth<500){
-            return Container(
-              width: 400,
-              height: 400,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.redAccent,
-                border: Border.all(
-                color: Colors.black,
-                width: 5
-              )
-            ),
-              child: Text("Normal Device"),
-          );
-        } else if(constraints.maxWidth<700){
-            return Container(
-                width: 500,
-                height: 500,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.purpleAccent,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 5
-                  )
-                ),
-                  child: Text("Tablet Device"),
-            );
-        } else if(constraints.maxWidth<1200){
-            return Container(
-              width: 600,
-              height: 600,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.yellowAccent,
-                border: Border.all(
-                  color: Colors.black,
-                  width: 5
-                )
-              ),
-                child: Text("Laptop Device"),
-            );
-        } else {
-            return Container(
-              width: 700,
-              height: 700,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.greenAccent,
-                border: Border.all(
-                  color: Colors.black,
-                  width: 5
-                )
-              ),
-                child: Text("Too Large"),
-            );
-          }
-      }
+      body: Wrap(
+        ///Wrap is an alternative widget of Row..it's similar to our notebooks..
+        ///it will work like row..the contents will stay horizentally..
+        ///but if there is no space in the screen it the content will appear in the next line..
+        alignment: WrapAlignment.center,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          Text("Flutter is very promising framework in 2024 "),
+          Text("Dart has already bitten Java and Kotlin in android development "),
+          Text("Regular Problem solving will grow the skills of critical thinking")
+        ],
       ),
     );
   }
