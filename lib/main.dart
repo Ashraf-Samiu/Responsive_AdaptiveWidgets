1c2744cc432e794cd82b2789f03c60a90c8f352b
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-08 04:22:17 +0600

    Orientation_Builder Commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,18 +18,33 @@ class HomeScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text("Home"),
       ),
-      body: Wrap(
-        ///Wrap is an alternative widget of Row..it's similar to our notebooks..
-        ///it will work like row..the contents will stay horizentally..
-        ///but if there is no space in the screen it the content will appear in the next line..
-        alignment: WrapAlignment.center,
-        crossAxisAlignment: WrapCrossAlignment.center,
-        children: [
-          Text("Flutter is very promising framework in 2024 "),
-          Text("Dart has already bitten Java and Kotlin in android development "),
-          Text("Regular Problem solving will grow the skills of critical thinking")
-        ],
-      ),
+      body: OrientationBuilder(builder: (context,orientation){
+        return Center(
+          ///using conditional operator
+          child: Container(
+            width: 300,
+            height: 200,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: Colors.redAccent,
+                border: Border.all(
+                color: Colors.black,
+                width: 5
+              )
+            ),
+            child: Text(
+                orientation==Orientation.portrait?
+                "Potrait Screen":
+                "LandScape",
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w200
+                ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
