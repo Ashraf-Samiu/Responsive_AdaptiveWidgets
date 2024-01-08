2e504d63170e710e5956025897b8a6e649bbe92c
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-09 00:05:40 +0600

    Positioned_Align Commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,106 +18,40 @@ class HomeScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text("Home"),
       ),
-      ///Stack follow the stack data structure...
       body: Center(
         child: Stack(
           children: [
             Container(
               width: 100,
               height: 100,
-              decoration: BoxDecoration(
-                color: Colors.greenAccent,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.greenAccent,
-                      blurRadius: 4,
-                      offset: Offset(4, 5)
-                    ),
-                    BoxShadow(
-                        color: Colors.lightBlue,
-                        blurRadius: 4,
-                        offset: Offset(4, 5)
-                    ),
-                    BoxShadow(
-                        color: Colors.purpleAccent,
-                        blurRadius: 4,
-                        offset: Offset(4, 5)
-                    ),
-                  ]
-              ),
+              color: Colors.pink,
+            ),
+            ///fill will cover the whole area..we can position it with align widget
+            Positioned.fill(
+                child: Align(
+                  ///by default it will take that widget in the center..(Alignment.center)
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    width: 80,
+                    height: 80,
+                    color: Colors.green,
+                  ),
+                ),
             ),
             Container(
-              width: 90,
-              height: 90,
-              decoration: BoxDecoration(
-                color: Colors.blueAccent,
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.blueAccent,
-                        blurRadius: 4,
-                        offset: Offset(3, 4)
-                    ),
-                    BoxShadow(
-                        color: Colors.redAccent,
-                        blurRadius: 4,
-                        offset: Offset(4, 5)
-                    ),
-                    BoxShadow(
-                        color: Colors.white38,
-                        blurRadius: 4,
-                        offset: Offset(4, 5)
-                    ),
-                  ]
-              ),
+              width: 60,
+              height: 60,
+              color: Colors.purple,
             ),
-            Container(
-              width: 80,
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.yellowAccent,
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.yellowAccent,
-                        blurRadius: 4,
-                        offset: Offset(2, 3)
-                    ),
-                    BoxShadow(
-                        color: Colors.greenAccent,
-                        blurRadius: 4,
-                        offset: Offset(4, 5)
-                    ),
-                    BoxShadow(
-                        color: Colors.purpleAccent,
-                        blurRadius: 4,
-                        offset: Offset(4, 5)
-                    ),
-                  ]
+            Positioned(
+              top: 5,
+              left: 5,
+              child: Container(
+                width: 40,
+                height: 40,
+                color: Colors.black,
               ),
-            ),
-            Container(
-              width: 70,
-              height: 70,
-              decoration: BoxDecoration(
-                color: Colors.purpleAccent,
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.purpleAccent,
-                        blurRadius: 4,
-                        offset: Offset(1, 2)
-                    ),
-                    BoxShadow(
-                        color: Colors.lightBlue,
-                        blurRadius: 4,
-                        offset: Offset(4, 5)
-                    ),
-                    BoxShadow(
-                        color: Colors.deepOrange,
-                        blurRadius: 4,
-                        offset: Offset(4, 5)
-                    ),
-                  ]
-              ),
-            ),
+            )
           ],
         ),
       ),
