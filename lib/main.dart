86a53103e2801f6b0e1662c7566e1478ebb159f1
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-08 08:42:17 +0600

    Aspect_Ratio Commit

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,10 @@ class HomeScreen extends StatelessWidget{
         height: 300,
         color: Colors.redAccent,
         alignment: Alignment.center,
+        ///Ratio:= Width:Height
+        ///Ratio:= 16:16 (16/16)-(means width and heights are equal)
+        ///Ratio:= 16:9 (16/9)- (means if width will be 160px the height will be 90px)
+        ///100 : 100
         child: AspectRatio(
             aspectRatio: 16/9,
             child: Container(
