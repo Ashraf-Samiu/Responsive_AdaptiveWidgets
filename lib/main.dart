63574c9a7525c8290a9a624cf1833c8126f80550
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-09 00:19:46 +0600

    Device_Preview Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
-import 'package:flutter/material.dart';
+import 'dart:js';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 void main(){
-  runApp(MyApp());
+  runApp(
+      DevicePreview(
+        ///we will disable it on production..(enabled: false)
+        enabled: true,
+          ///If we use KDebugMode,The App will show devicePreview in the bottom..after releasing the app the option will be disappeared..
+          ///kReleaseMode is opposite of it's..
+          builder: (context){
+            return MyApp();
+      }));
 }
 class MyApp extends StatelessWidget{
   @override
@@ -17,43 +28,6 @@ class HomeScreen extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
-      ),
-      body: Center(
-        child: Stack(
-          children: [
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.pink,
-            ),
-            ///fill will cover the whole area..we can position it with align widget
-            Positioned.fill(
-                child: Align(
-                  ///by default it will take that widget in the center..(Alignment.center)
-                  alignment: Alignment.topCenter,
-                  child: Container(
-                    width: 80,
-                    height: 80,
-                    color: Colors.green,
-                  ),
-                ),
-            ),
-            Container(
-              width: 60,
-              height: 60,
-              color: Colors.purple,
-            ),
-            Positioned(
-              top: 5,
-              left: 5,
-              child: Container(
-                width: 40,
-                height: 40,
-                color: Colors.black,
-              ),
-            )
-          ],
-        ),
       ),
     );
   }
