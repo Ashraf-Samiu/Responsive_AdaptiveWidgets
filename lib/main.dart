39e0c0611ff8b946d3427274f39d38fcf0bd5232
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-09 00:24:08 +0600

    Device_Preview Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:flutter/foundation.dart';///kDebugMode,kReleaseMode are part of it
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 void main(){
   runApp(
       DevicePreview(
-        ///we will disable it on production..(enabled: false)
-        enabled: true,
-          ///If we use KDebugMode,The App will show devicePreview in the bottom..after releasing the app the option will be disappeared..
+        ///we will disable it on production..(enabled: true=on/false=off)
+        enabled: kDebugMode,
+          ///If we use kDebugMode,The App will show devicePreview in the bottom..after releasing the app the option will be disappeared..
           ///kReleaseMode is opposite of it's..
           builder: (context){
             return MyApp();
